8ab4f12c7f1c733bfae4e0e4dc459d6c39349445
hari2796 <harinadhpalli3@gmail.com>
2026-04-16 09:59:03 +0530

    first commit

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <div>
+        <Link to='/'>Home</Link><br/>
+        <Link to='/login'>Login</Link><br/>
+        <Link to='/register'>Register</Link>
+      
+    </div>
+  )
+}
+
+
+
+
+
