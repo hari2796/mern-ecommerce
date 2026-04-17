615121de132ec2d4fbda4f2c1b76ab87d67de615
hari2796 <harinadhpalli3@gmail.com>
2026-04-17 17:10:02 +0530

    deployed url updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,7 @@ const express= require("express")
 const router=express.Router() 
 const bcrypt=require("bcrypt")
 const jwt= require("jsonwebtoken")
-const User= require("../models/user")
-
+const User= require("../models/User")
 router.post("/register",async (req,res)=>{
     try{
         const {name,email,password,mobile,role}=req.body 
